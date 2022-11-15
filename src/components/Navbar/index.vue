824189ff7013d4336443ee797fcb33ae4e0a2be7
orzorzooo <orzorzooo@gmail.com>
2022-11-15 20:35:21 +0800

    navbar變數

--- src/components/Navbar/index.vue
+++ src/components/Navbar/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <div
-    ref="navbar"
-    class="transition fixed w-full z-60 bg-gray-100 bg-opacity-0 text-white"
-  >
+  <div ref="navbar" class="transition fixed w-full z-60 bg-gray-100 bg-opacity-0 text-white">
     <div class="h-20">
       <div class="flex h-full px-20 justify-between items-center">
-        <div class="text-2xl font-bold">APDC</div>
-        <div class="text-lg">APDC</div>
+        <div class="text-2xl font-bold">{{ company_name }}</div>
+        <div class="text-lg">{{ company_name }}</div>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      company_name: "PANDA",
+    };
+  },
   methods: {
     onScrollTrigger(navbar, value) {
       if (window.pageYOffset > value) {
