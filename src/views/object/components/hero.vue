1c6c5dbf5bb3d636cbab7f00e1009914151f83a1
orzorzooo <orzorzooo@gmail.com>
2022-11-23 10:31:51 +0800

    EDIT: hero 模糊底功能

--- src/views/object/components/hero.vue
+++ src/views/object/components/hero.vue
@@ -1,18 +1,35 @@
 <template>
   <v-container fluid class="p-0">
-    <v-img :src="featureImg" height="100vh" gradient="to bottom right, rgba(0,0,0,.9), rgba(0,0,0,.3)">
+    <v-img
+      :src="featureImg"
+      height="100vh"
+      gradient="to bottom right, rgba(0,0,0,.9), rgba(0,0,0,.3)"
+    >
       <v-container class="h-full">
         <v-row align="center" class="h-full p-10">
           <v-col cols="12" sm="12" md="6">
-            <div class="text text-yellow-400/80 mb-3" data-aos="fade-right">
-              {{ property.address }}
-            </div>
-            <div class="text-5xl text-white" data-aos="fade-right" data-aos-delay="300">
-              {{ property.name }}
-            </div>
+            <div :class="headerBlur ? headerBlurClass : ''" data-aos="fade">
+              <div class="text text-yellow-400/80 mb-3" data-aos="fade-right">
+                {{ property.address }}
+              </div>
+              <div
+                class="text-5xl text-white"
+                data-aos="fade-right"
+                data-aos-delay="300"
+              >
+                {{ property.name }}
+              </div>
 
-            <HeaderList :title="'租金'" :content="`$${property.rent_price}/月`"></HeaderList>
-            <HeaderList :delay="300" :title="'地坪'" :content="`${property.square_meter}坪`"></HeaderList>
+              <HeaderList
+                :title="'租金'"
+                :content="`$${property.rent_price}/月`"
+              ></HeaderList>
+              <HeaderList
+                :delay="300"
+                :title="'地坪'"
+                :content="`${property.square_meter}坪`"
+              ></HeaderList>
+            </div>
           </v-col>
           <v-col sm="12"> </v-col>
         </v-row>
@@ -29,8 +46,19 @@ export default {
   computed: {
     featureImg() {
       if (this.property.files.length == 0) return;
-      return assetURL(this.property.files[0].directus_files_id);
+      return assetURL(this.property.files[0].directus_files_id, {
+        quality: 50,
+        width: 1920,
+        // transforms: `[["blur",7]]`,
+      });
     },
+  },
+  data() {
+    return {
+      headerBlur: false,
+      headerBlurClass:
+        "bg-gray-100/10 rounded-xl p-5 backdrop-filter backdrop-blur-sm border-1 border-gray-100/10 h-60vh mt-30",
+    };
   },
 };
 </script>
