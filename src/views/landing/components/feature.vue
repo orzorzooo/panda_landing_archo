0896982fae02d85255cdc1c891ca1beacd7dde50
orzorzooo <orzorzooo@gmail.com>
2022-11-24 16:36:22 +0800

    EDIT: landing feature特效

--- src/views/landing/components/feature.vue
+++ src/views/landing/components/feature.vue
@@ -2,50 +2,38 @@
   <div>
     <v-container fluid class="p-0">
       <div class="h-150">
-        <div class="h-full">
-          <transition name="slide-fade">
-            <div :style="bg()" class="h-full img">
-              <v-row class="h-full" no-gutters>
-                <v-col
-                  :cols="6"
-                  :md="3"
-                  v-for="(item, i) in features"
-                  :key="i"
-                  class="border-r-1 border-b-1 on-hover p-0 text-black text-2xl font-bold"
-                  @mouseenter="hover = i"
-                >
-                  <v-expand-transition>
-                    <!-- <v-icon v-if="hover == i"> mdi-plus-circle </v-icon> -->
-                    <div v-if="hover == i" class="bg-white <md:h-full md:h-30">
-                      <div class="p-10 pt-3">
-                        <div class="text-sm mb-3">service</div>
-                        <div class="">{{ item.title }}</div>
-                      </div>
-                    </div>
-                  </v-expand-transition>
-                  <div class="p-10 pt-3 text-white" v-if="hover != i">
-                    <div class="text-sm mb-3">service</div>
-                    <div class="">{{ item.title }}</div>
-                  </div>
-                </v-col>
-              </v-row>
-            </div>
-            <!-- <v-img :src="features[hover].image" height="100%">
-            <v-row class="h-full p-0">
-              <v-col
-                :cols="6"
-                :md="3"
-                v-for="(item, i) in features"
-                :key="i"
-                class="p-10 border-r-1 border-t-1"
-                @mouseenter="hover = i"
-              >
-                <div>{{ item.title }}</div>
-              </v-col>
-            </v-row>
-          </v-img> -->
+        <div class="absolute h-150 w-full overflow-hidden bg-white">
+          <transition name="fade">
+            <img
+              :src="features[hover].image"
+              :key="hover"
+              alt=""
+              class="max-h-full w-full object-cover h-150 transition-opacity"
+            />
           </transition>
         </div>
+
+        <v-row no-gutters class="relative h-full">
+          <v-col
+            v-for="(item, i) in features"
+            :key="i"
+            @mouseenter="hover = i"
+            class="border-r-1 border-b-1 on-hover p-0 text-black text-2xl font-bold on-hover"
+          >
+            <v-expand-transition>
+              <div v-if="hover == i" class="bg-white <md:h-full md:h-30">
+                <div class="p-10 pt-3">
+                  <div class="text-sm mb-3">service</div>
+                  <div class="">{{ item.title }}</div>
+                </div>
+              </div>
+            </v-expand-transition>
+            <div class="p-10 pt-3 text-white" v-if="hover != i">
+              <div class="text-sm mb-3">service</div>
+              <div class="">{{ item.title }}</div>
+            </div>
+          </v-col>
+        </v-row>
       </div>
     </v-container>
   </div>
@@ -63,23 +51,19 @@ export default {
       features: [
         {
           title: "租賃",
-          image:
-            "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          image: require("@/assets/images/landing-feature-01.jpg"),
         },
         {
           title: "買賣",
-          image:
-            "https://images.pexels.com/photos/189333/pexels-photo-189333.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          image: require("@/assets/images/landing-feature-02.jpg"),
         },
         {
           title: "出租",
-          image:
-            "https://images.pexels.com/photos/1484981/pexels-photo-1484981.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          image: require("@/assets/images/landing-feature-03.jpg"),
         },
         {
           title: "輕鋼構",
-          image:
-            "https://images.pexels.com/photos/2029667/pexels-photo-2029667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          image: require("@/assets/images/landing-feature-04.jpg"),
         },
       ],
     };
@@ -87,21 +71,23 @@ export default {
 };
 </script>
 <style scoped>
-.img {
-  background-size: cover;
-  -webkit-transition: all 0.3s ease;
-  transition: all 0.3s ease;
-}
-/* prefix with transition name */
-.slide-fade-enter-active {
-  opacity: 1;
-  z-index: 10;
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease-in-out;
+  /* transition: 0.5s -webkit-filter linear; */
 }
 
-.slide-fade-leave-active {
+.fade-leave,
+.fade-enter-to {
   opacity: 1;
+  filter: blur(20px);
 }
 
+.fade-enter,
+.fade-leave-to {
+  opacity: 1;
+  filter: blur(20px);
+}
 .on-hover {
   background-color: rgba(0, 0, 0, 0.2);
 }
