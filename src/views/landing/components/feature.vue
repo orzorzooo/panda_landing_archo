f76e5b5c16044aea575534206686c177bf8ecd58
orzorzooo <orzorzooo@gmail.com>
2022-11-25 09:40:16 +0800

    EDIT: 優化RWD效果

--- src/views/landing/components/feature.vue
+++ src/views/landing/components/feature.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <v-container fluid class="p-0">
-      <div class="h-150">
-        <div class="absolute h-150 w-full overflow-hidden bg-white">
+      <div class="<md:h-100 h-150">
+        <div class="absolute <md:h-100 h-150 w-full overflow-hidden bg-white">
           <transition name="fade">
             <img
               :src="features[hover].image"
@@ -17,11 +17,31 @@
           <v-col
             v-for="(item, i) in features"
             :key="i"
-            @mouseenter="hover = i"
             @click="hover = i"
+            @mouseover="hover = i"
             class="border-r-1 border-b-1 on-hover p-0 text-black text-2xl font-bold on-hover"
+            cols="6"
+            md="3"
           >
-            <v-expand-transition>
+            <div class="absolute p-10 pt-3 t-0 text-white" v-if="hover != i">
+              <div class="text-sm mb-3">service</div>
+              <div class="">{{ item.title }}</div>
+            </div>
+            <v-fade-transition
+              v-if="$vuetify.breakpoint.xsOnly || $vuetify.breakpoint.smOnly"
+            >
+              <div
+                v-if="hover == i"
+                class="bg-white <md:h-full md:h-30 text-black"
+              >
+                <div class="absolute p-10 pt-3 t-0">
+                  <div class="text-sm mb-3">service</div>
+                  <div class="">{{ item.title }}</div>
+                </div>
+              </div>
+            </v-fade-transition>
+
+            <v-expand-transition v-else>
               <div v-if="hover == i" class="bg-white <md:h-full md:h-30">
                 <div class="p-10 pt-3">
                   <div class="text-sm mb-3">service</div>
@@ -29,10 +49,6 @@
                 </div>
               </div>
             </v-expand-transition>
-            <div class="p-10 pt-3 text-white" v-if="hover != i">
-              <div class="text-sm mb-3">service</div>
-              <div class="">{{ item.title }}</div>
-            </div>
           </v-col>
         </v-row>
       </div>
@@ -74,20 +90,20 @@ export default {
 <style scoped>
 .fade-enter-active,
 .fade-leave-active {
-  transition: all 0.3s ease-in-out;
+  transition: all 0.2s linear;
   /* transition: 0.5s -webkit-filter linear; */
 }
 
 .fade-leave,
 .fade-enter-to {
-  opacity: 1;
+  opacity: 0.5;
   filter: blur(20px);
 }
 
 .fade-enter,
 .fade-leave-to {
   opacity: 1;
-  filter: blur(20px);
+  filter: blur(10px);
 }
 .on-hover {
   background-color: rgba(0, 0, 0, 0.2);
