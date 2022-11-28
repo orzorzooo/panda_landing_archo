2c4356e72005968364341b1647ab9b8fba7033db
orzorzooo <orzorzooo@gmail.com>
2022-11-28 21:34:43 +0800

    ADD: views/user 架構

---
+++ src/views/user/home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <div class="text-8xl font-bold text-white">home</div>
+  </div>
+</template>
---
+++ src/views/user/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-full bg-gray-700">
+    <v-container>
+      <router-view />
+    </v-container>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters("user", ["user"]),
+  },
+};
+</script>
