566d79ac7b8d28abe32add6fea3dfbcb770b2629
orzorzooo <orzorzooo@gmail.com>
2022-11-28 21:45:45 +0800

    EDIT: nav user路由

--- src/components/Navbar/index.vue
+++ src/components/Navbar/index.vue
@@ -1,23 +1,12 @@
 <template>
-  <div
-    ref="navbar"
-    class="transition fixed w-full z-60 bg-gray-100 bg-opacity-0 text-white"
-  >
+  <div ref="navbar" class="transition fixed w-full z-60 bg-gray-100 bg-opacity-0 text-white">
     <!-- standard -->
     <div class="h-20">
-      <div
-        class="flex h-full px-20 justify-between items-center"
-        v-if="$vuetify.breakpoint.mdAndUp"
-      >
-        <NavItem :routeName="'landing'"
-          ><div class="text-2xl font-bold">PANDA</div></NavItem
-        >
+      <div class="flex h-full px-20 justify-between items-center" v-if="$vuetify.breakpoint.mdAndUp">
+        <NavItem :routeName="'landing'"><div class="text-2xl font-bold">PANDA</div></NavItem>
         <v-spacer></v-spacer>
 
-        <NavItem
-          v-for="(item, i) in routes"
-          :key="i"
-          :routeName="item.routeName"
+        <NavItem v-for="(item, i) in routes" :key="i" :routeName="item.routeName"
           ><div class="text-xl font-bold">{{ item.title }}</div></NavItem
         >
       </div>
@@ -25,25 +14,18 @@
       <!-- MOBILE -->
 
       <div v-else class="flex h-full w-full px-5 items-center">
-        <NavItem :routeName="'landing'"
-          ><div class="text-2xl font-bold">PANDA</div></NavItem
-        >
+        <NavItem :routeName="'landing'"><div class="text-2xl font-bold">PANDA</div></NavItem>
         <v-spacer></v-spacer>
-        <v-app-bar-nav-icon
-          :dark="!scroll"
-          class=""
-          @click="drawer = true"
-        ></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon :dark="!scroll" class="" @click="drawer = true"></v-app-bar-nav-icon>
       </div>
       <v-navigation-drawer v-model="drawer" right absolute dark height="100vh">
         <div class="w-full py-10">
           <v-list>
             <v-list-item link>
-              <NavItem :routeName="'landing'"
-                ><div class="text-xl font-bold text-orange-200/70">
-                  首頁
-                </div></NavItem
-              >
+              <NavItem :routeName="'landing'"><div class="text-xl font-bold text-orange-200/70">首頁</div></NavItem>
+            </v-list-item>
+            <v-list-item link v-if="user">
+              <NavItem :routeName="'home'"><div class="text-xl font-bold text-orange-200/70">我的物件</div></NavItem>
             </v-list-item>
             <v-divider dark class="mb-5"></v-divider>
             <v-list-item link v-for="(item, i) in routes" :key="i">
@@ -61,6 +43,7 @@
 </template>
 <script>
 import NavItem from "./components/navItem.vue";
+import { mapGetters } from "vuex";
 export default {
   components: { NavItem },
   data() {
@@ -84,6 +67,9 @@ export default {
         this.scroll = false;
       }
     },
+  },
+  computed: {
+    ...mapGetters("user", ["user"]),
   },
   mounted() {
     const _this = this;
