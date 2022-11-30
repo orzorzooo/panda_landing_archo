56d76ddbc82d195ab059aa7c1ca6d796979f49f1
orzorzooo <orzorzooo@gmail.com>
2022-11-30 17:08:31 +0800

    ADD:properties 架構

--- src/views/user/home.vue
+++ src/views/user/home.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <div class="text-8xl font-bold text-white h-500">home</div>
+    <!-- <div class="text-8xl font-bold text-white h-500">home</div> -->
+    <div>
+      <v-container> </v-container>
+    </div>
   </div>
 </template>
+<script>
+import { get, post } from "@/api/request";
+export default {};
+</script>
